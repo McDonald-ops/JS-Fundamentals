1f9bad7fd6be5916d8bc82df6f0a8300d9c56758
McDonald <odiasemcdonald1@gmail.com>
2025-05-24 19:19:03 +0100

    Add script that prints 3 lines using an array and a loop

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+const lines = [`C is fun`, `Python is cool`, `JavaScript is amazing`];
+// Loop through the array and print each line
+// using a for loop
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
+}

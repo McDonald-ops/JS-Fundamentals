29ba11ff5a2045972f6d27290dc5eafaa4eb3b1e
McDonald <odiasemcdonald1@gmail.com>
2025-05-24 19:38:06 +0100

    Print 'C is fun' x times based on user input

---
+++ 7-multi_c.js
@@ -0,0 +1,9 @@
+const x = parseInt(process.argv[2], 10);
+
+if (isNaN(x)) {
+  console.log("missing number of occurrences");
+} else {
+    for (let i = 0; i < x; i++) {
+        console.log("C is fun");
+    }
+}

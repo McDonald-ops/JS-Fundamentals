0510db762bc6bf042cc15b1f3e399027c34d4d9e
McDonald <odiasemcdonald1@gmail.com>
2025-05-25 18:25:12 +0100

    Add script that converts the first argument to an integer

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -2,7 +2,7 @@ const arg = process.argv[2];
 // This code takes a command line argument, converts it to an integer, and checks if it is a valid number.
 
 if (!isNaN(parseInt(arg))) {
-  console.log("my number:" + parseInt(arg));
+  console.log("My number:" + parseInt(arg));
 } else {
   console.log("Not a number");
-}
+} 

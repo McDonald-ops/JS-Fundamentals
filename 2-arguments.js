5524e1ec1c765262bb542c1e60d1a80f1388bea9
McDonald <odiasemcdonald1@gmail.com>
2025-05-25 18:39:58 +0100

    Updated script to print arguments passed to it

--- 2-arguments.js
+++ 2-arguments.js
@@ -2,7 +2,7 @@ const args = process.argv.slice(2);
 // Remove the first two elements (node and script path)
 
 if (args.length === 0) {
-  console.log("No arguments");
+  console.log("No argument");
 } else if (args.length === 1) {
   console.log("Argument found");
 } else {
